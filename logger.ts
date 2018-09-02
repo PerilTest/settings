f7b150cc8cb765951b0d4914a0a009bf8d0f2e54
Orta <orta.therox+github@gmail.com>
2018-09-02 09:43:28 -0400

    Update logger.ts

--- logger.ts
+++ logger.ts
@@ -3,19 +3,6 @@ console.log("Hi!")
 import { peril } from "danger"
 import { IncomingWebhook } from "@slack/client"
 
-/**
- * A task that accepts Slack incoming webhook data
- * and sends a message into the Artsy Dev chat room.
- *
- * The full API docs for the syntax of the expected data
- * can be found: https://slackapi.github.io/node-slack-sdk/reference/IncomingWebhook
- * 
- * Usage in a Dangerfile:
- * 
-
-    peril.runTask("slack-dev-channel", "in 5 minutes", message)
- */
-
 export default (data: any) => {
     const message = {
       unfurl_links: false,
