85f5e0cdae83f46b70972f76905c21fe7e1360ec
Orta <orta.therox+github@gmail.com>
2018-09-02 09:25:33 -0400

    Update scheduleNow.ts

--- scheduleNow.ts
+++ scheduleNow.ts
@@ -1,7 +1,30 @@
 import { peril, danger } from "danger"
 import { Issues } from "github-webhook-event-types"
 
+import { IncomingWebhook } from "@slack/client"
+
 export default async (issues: Issues) => {
-  console.log("Doing some work")
-  await peril.runTask("logger", "in 5 minutes", {})
+  
+  await peril.runTask("logger", "in 1 minutes", { time: 1 })
+  await peril.runTask("logger", "in 5 minutes", { time: 2 })
+  await peril.runTask("logger", "in 10 minutes", { time: 10 })
+  await peril.runTask("logger", "in 20 minutes", { time: 20 })
+  
+  console.log("Hi!")
+  const message = {
+    unfurl_links: false,
+    attachments: [
+      {
+        color: "good",
+        title: "Triggered a bunch of loggers",
+      },
+    ],
+  }
+
+  const url = peril.env.SLACK_RFC_WEBHOOK_URL || ""
+  const webhook = new IncomingWebhook(url)
+  await webhook.send(data)
+
+  console.log("Triggering the logger")
+  
 }
