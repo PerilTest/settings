680a3e2a7e45e884b4ed62748f39d496e72f210e
Orta Therox <orta.therox@gmail.com>
2019-02-17 18:48:32 -0500

    Log

--- logStuff.ts
+++ logStuff.ts
@@ -1,8 +1,10 @@
-import {danger, message} from "danger"
+import { danger, message } from "danger"
+import { IncomingWebhook } from "@slack/client"
 
 export default async (things: any) => {
   console.log(danger)
   console.log(things)
+  console.log(IncomingWebhook)
 }
 
 console.log("Go")
